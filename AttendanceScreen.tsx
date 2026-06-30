d0d5e15a1abcb9fb5537a004914c456133cad858
oresdeveloper-glitch <oresdeveloper@gmail.com>
2026-06-30 17:48:45 +0300

    Update AttendanceScreen.tsx

--- AttendanceScreen.tsx
+++ AttendanceScreen.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import MustPageBackground from '../components/MustPageBackground';
-import { useApp } from '../context/AppContext';
-import { cn } from '../utils/cn';
+import MustPageBackground from 'MustPageBackground';
+import { useApp } from 'AppContext';
+import { cn } from 'cn';
 import { ClipboardCheck, QrCode, UserCheck, Clock, TrendingUp, CheckCircle, XCircle, AlertCircle } from 'lucide-react';
 
 export default function AttendanceScreen() {
